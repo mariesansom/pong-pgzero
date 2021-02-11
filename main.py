128a0aa4e1e65f2a53e128d6e8d1b4c4e876a8ad
Marie Quinn Sansom <marieq@Maries-MacBook-Pro.local>
2021-02-11 15:04:23 -0500

    Created the second red paddle and gave it up and down functionality.

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ TITLE = 'Pong game'
 
 WHITE = 255, 255, 255
 BLUE = 0, 0, 255
+RED = 255, 0, 0 
 BALL = Rect((WIDTH/2, HEIGHT/2), (10,10))
 PADDLE_LENGTH = 100
 PADDLE1 = Rect((20, 20),(10, PADDLE_LENGTH))
-# PADDLE2 =
+PADDLE2 = Rect((570, 20),(10, PADDLE_LENGTH))
 
 def update():
     if keyboard.down and PADDLE1.y + PADDLE_LENGTH < HEIGHT - 20:
         PADDLE1.y += 10
     elif keyboard.up and PADDLE1.y > 20:
         PADDLE1.y -= 10
+    
+    if keyboard.a and PADDLE2.y + PADDLE_LENGTH < HEIGHT - 20:
+         PADDLE2.y += 10
+    elif keyboard.c and PADDLE2.y > 20:
+        PADDLE2.y -= 10
 
 def draw():
     screen.clear()
     screen.draw.filled_rect(BALL, WHITE)
-    screen.draw.filled_rect(PADDLE1, BLUE)   
+    screen.draw.filled_rect(PADDLE1, BLUE)
+    screen.draw.filled_rect(PADDLE2, RED)   
 
 
 
